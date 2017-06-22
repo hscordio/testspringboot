7fffc901a60d5a15bac14306ab8198ce4450a95a
hscordio <howard.scordio@soprasteria.com>
2017-06-23 01:08:22 +0200

    fixed import and added key store and trust store loading

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
-import com.sun.net.ssl.SSLContext;
 import javax.net.ssl.HttpsURLConnection;
-import com.sun.net.ssl.TrustManager;
-import com.sun.net.ssl.TrustManagerFactory;
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,15 +23,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.security.KeyStore;
 import java.security.SecureRandom;
-import com.sun.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
 
 
 
@@ -63,28 +65,43 @@ public class HelloController {
 //        PagedList<Post> feed = facebook.feedOperations().getFeed();
 //        model.addAttribute("feed", feed);
 //        return "hello";
+//    	String absolutePath = companiesXml.getFile().getAbsolutePath();
+//    	System.setProperty("javax.net.ssl.trustStore", absolutePath);
+//	    System.setProperty("javax.net.ssl.trustStorePassword","changeit");
+
     	
+    	String filename = System.getProperty("java.home") + "/lib/security/cacerts".replace('/', File.separatorChar);
+//		System.setProperty("javax.net.ssl.keyStore", filename);
+//	    System.setProperty("javax.net.ssl.keyStorePassword","changeit");
+	    
     	try {
+	    
     	InputStream inputStream = companiesXml.getInputStream();
-    	KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
-    	keystore.load(inputStream, "changeit".toCharArray());
-
-
-    	SSLContext context = SSLContext.getInstance("TLSv1.2");
-
-    	final TrustManagerFactory tmf = TrustManagerFactory.getInstance("X509");
-    	tmf.init(keystore);
+    	KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+    	trustStore.load(inputStream, "changeit".toCharArray());
+    	logger.info("number of certificate found in trustStore: " + trustStore.size());
+    	final TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+    	tmf.init(trustStore);
     	final TrustManager[] trustManagers = tmf.getTrustManagers();
 
-    	context.init(null, trustManagers, new SecureRandom());
+    	FileInputStream fis = new FileInputStream(filename);
+    	KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+    	keyStore.load(fis, "changeit".toCharArray());
+    	logger.info("number of certificate found in keystore: " + keyStore.size());
+    	KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+    	kmf.init(keyStore,"changeit".toCharArray());
+    	final KeyManager[] keyManagers = kmf.getKeyManagers();
+    	
+    	SSLContext context = SSLContext.getInstance("TLSv1.2");
+    	context.init(keyManagers, trustManagers, new SecureRandom());
 
     	HttpsURLConnection.setDefaultSSLSocketFactory(context.getSocketFactory());
-    	
     	
     	} catch (Exception e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 			logger.error("Errore nel settaggio del certificato"+e1);
+			return new ResponseEntity<String>("Errore nel settaggio del certificato: "+ e1, HttpStatus.OK);
 		}
     	
     	//System.setProperty("javax.net.ssl.trustStrore", "applepaykeystore.jks");
@@ -98,7 +115,7 @@ public class HelloController {
     	//});
 
     	ApplePayMerchantValidationRequestData merchantValidationRequest = new ApplePayMerchantValidationRequestData();
-    	merchantValidationRequest.setDisplayName("Oakley US");
+    	merchantValidationRequest.setDisplayName("Heroku Oakley Us Test Account");
     	merchantValidationRequest.setDomainName("testapplepayoakley.herokuapp.com");
     	merchantValidationRequest.setMerchantIdentifier("merchant.us.oakley.com.heroku");
 
